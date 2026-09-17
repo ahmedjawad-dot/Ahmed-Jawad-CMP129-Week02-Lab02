41ada070ebff36f954b84d0099703b2d9c8ba325
ahmedjawad-dot <ahmed.jawad@student.ccm.edu>
2026-09-17 15:34:38 -0400

    Completed Lab Phase 1

--- Person.java
+++ Person.java
@@ -1,3 +1,39 @@
 public class Person {
     
+    private String name;
+    private int age;
+    private String email;
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String n){
+        name = n;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public void setAge(int a){
+        age = a;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public void setEmail(String e){
+        email = e;
+    }
+
+    public void displayInfo(){
+        System.out.println("Person Information");
+        System.out.println("------------------");
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Email: " + email);
+        System.out.println();
+    }
 }
--- PersonTest.java
+++ PersonTest.java
@@ -1,3 +1,20 @@
 public class PersonTest {
-    
+    public static void main(String[] args) {
+
+        Person person = new Person();
+        person.setName("joe");
+        person.setAge(12);
+        person.setEmail("ahmed.jawad@student.ccm.edu");
+        person.displayInfo();
+
+        Person person2 = new Person();
+        person2.setName("Schmidley");
+        person2.setAge(15);
+        person2.setEmail("smith.schmidley@student.ccm.edu");
+        person2.displayInfo();      
+
+
+
+    }
+
 }
